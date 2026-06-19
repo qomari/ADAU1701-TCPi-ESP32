9d0e2401fb0962e9ba66ef9a81ff90a5e9ee8672
qomari <arhiabb919@gmail.com>
2026-06-19 22:22:07 +0700

    Add root sketch wrapper so Arduino IDE can open the project from repo root

---
+++ ADAU1701_TCPi_ESP32.ino
@@ -0,0 +1,5 @@
+// ADAU1701_TCPi_ESP32.ino (wrapper)
+// This root .ino lets Arduino IDE open the sketch directly.
+// It forwards to the original file located in the ADAU1701_TCPi_ESP32/ directory.
+
+#include "ADAU1701_TCPi_ESP32/ADAU1701_TCPi_ESP32.ino"
